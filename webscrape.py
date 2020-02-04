ccb51fd30825a020bcf666f112b9538ed7275dc3
adeelbhatti88 <36842562+adeelbhatti88@users.noreply.github.com>
2020-02-04 16:45:11 -0600

    Add files via upload

--- webscrape.py
+++ webscrape.py
@@ -24,7 +24,7 @@ def main_window1():
     entry.place(relwidth=0.65, relheight=1)
 
     # add a button
-    button = tk.Button(frame, text="Get Item Value", font=40, command=lambda: show_value(entry.get()))
+    button = tk.Button(frame, text="Get Item Value", font=40, command=lambda: show_value(entry.get(), label))
     button.place(relx=0.7, relheight=1, relwidth=0.3)
 
     lower_frame = tk.Frame(root, bg='blue', bd=10)
@@ -32,6 +32,8 @@ def main_window1():
 
     label = tk.Label(lower_frame)
     label.place(relwidth=1, relheight=1)
+
+
 
     tk.Button(root, text="Quit", command = root.destroy).pack()
 
@@ -62,9 +64,12 @@ for val in datajson['lines']:
     currency[val['currencyTypeName']] = val['receive']['value']
 
 print(currency)
-def show_value(entry):
+def show_value(entry, label):
     print("inside show value function!!", entry)
     print(currency.get(entry))
+    label['text'] = currency.get(entry)
+
+
 
 # canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
 # canvas.pack() #the .pack displays it on the screen
