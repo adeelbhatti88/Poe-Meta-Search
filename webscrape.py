4d3fdb29be79a3dbf3a3ab50d92bb853badff165
adeelbhatti88 <36842562+adeelbhatti88@users.noreply.github.com>
2020-02-13 00:26:08 -0600

    Add files via upload
    added chaos orb text to currency output

--- webscrape.py
+++ webscrape.py
@@ -5,6 +5,7 @@ import tkinter as tk
 
 def open_window():
     root = tk.Tk()
+    root.configure(bg="blue")
     button = tk.Button(root, text="currency search", command=main_window1)
     weaponButton = tk.Button(root, text="Weapons Search", command=main_window2)
     button.pack()
@@ -68,16 +69,6 @@ def main_window1():
 
 
 
-# root = tk.Tk()
-# button = tk.Button(root, text="currency search", command = main_window1)
-# button.pack()
-# root.geometry("400x400+150+150")
-# root.mainloop()
-#https://www.youtube.com/watch?v=D8-snVfekto continue from 14:19
-
-
-
-
 url = 'https://poe.ninja/api/data/currencyoverview?league=Metamorph&type=Currency&language=en'
 weaponUrl = 'https://poe.ninja/api/data/itemoverview?league=Metamorph&type=UniqueWeapon&language=en'
 
@@ -101,41 +92,18 @@ for val in weaponJson['lines']:
 
 print(currency)
 def show_value(entry, label):
+    chaosOrbs = ' ChaosOrbs'
     print("inside show value function!!", entry)
     print(currency.get(entry))
-    label['text'] = currency.get(entry)
+    label['text'] = str(currency.get(entry)) + chaosOrbs
+
+
 
 def show_weaponValue(entry, label):
-    label['text'] = weapons.get(entry)
-
-
-
-# canvas = tk.Canvas(root, height=HEIGHT, width = WIDTH)
-# canvas.pack() #the .pack displays it on the screen
-#
-#
-#
-# frame = tk.Frame(root, bg='blue', bd=5)
-# frame.place(relx=0.5, rely=0.1, relwidth=0.75, relheight=0.1, anchor='n')
-#
-# entry = tk.Entry(frame, font=40)
-# entry.place(relwidth=0.65, relheight=1)
-#
-# #add a button
-# button = tk.Button(frame, text="Get Item Value", font=40, command=lambda: show_value(entry.get()))
-# button.place(relx=0.7, relheight=1, relwidth=0.3)
-#
-# lower_frame = tk.Frame(root, bg='blue', bd=10)
-# lower_frame.place(relx=0.5, rely=0.25, relwidth=0.75, relheight=0.6, anchor='n')
-#
-# label = tk.Label(lower_frame)
-# label.place(relwidth=1, relheight=1)
+    chaosOrbs = ' ChaosOrbs'
+    label['text'] = str(weapons.get(entry)) + chaosOrbs
 
 
 open_window()
 
-#
-#
-#
-#
-# root.mainloop()
+
